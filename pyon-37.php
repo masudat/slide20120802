08b82989a5e2071674a191c25c5d7ced2265b7dc
37 <t10792fm@sfc.keio.ac.jp>
2012-08-02 00:52:55 +0900

    change(pyon-37.php)

--- pyon-37.php
+++ pyon-37.php
@@ -1,7 +1,23 @@
 <div id="p3-1" class="step test" data-x="9000" data-y="3000" data-scale="2">
-  <p>This is sample for aa.</p>
-  <ul><li>hoge</li><li>hoge</li></ul>
+  <p></p>
+  <h2>gitの実装</h2>
+  <h3>内容</h3>
+  <ul>
+    <li>ローカル(add, commit)</li>
+    <li>ブランチ(create, check, switch)</li>
+    <li>リモート(pull, push, clone)</li>
+    <li>そのほか(easy commit)</li>
+  </ul>
 </div>
+
 <div id="p3-2" class="step test" data-x="11000" data-y="3000" data-scale="2">
-  <p>This is 2nd sample for aa.</p>
+  <h2>実装方法</h2>
+  <ul>
+    <li>dialogs, context menu, html</li>
+    <li>言語: js, php</li>
+  </ul>
+  <h2>課題</h2>
+  <ul>
+    <li>実装できたgitの機能が途中まで</li>
+  </ul>
 </div>
